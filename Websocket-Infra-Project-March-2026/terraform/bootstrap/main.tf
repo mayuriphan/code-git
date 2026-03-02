0ec01566b037c779b025bb86b163a17fa7ef40b3
Mayuri Phansalkar <mdp@bitadata.com>
2026-03-02 15:58:04 +0000

    encryption enabled and removed .tf cache

--- Websocket-Infra-Project-March-2026/terraform/bootstrap/main.tf
+++ Websocket-Infra-Project-March-2026/terraform/bootstrap/main.tf
@@ -23,6 +23,16 @@ resource "aws_s3_bucket_public_access_block" "block" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "aws_dynamodb_table" "terraform_lock" {
   name         = "may-app-terraform-lock-${var.environment}"
   billing_mode = "PAY_PER_REQUEST"
